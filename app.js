ca134bd9d256a6b80d85b54317218adecb496cca
thomascytang <thomascytang@gmail.com>
2018-09-17 20:53:01 +1000

    basic turn stuff

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ var play = io.of('/play');
 var usercount = 0;
 
 
+//keeps track of current running game
+var game = { players: [], running: false, currentPlayer: 0 };
+
+function startGame(){
+	game.running  = true;
+	play.to('players').emit('start game');
+	play.to(game.players[0]).emit('your turn', game.currentPlayer);
+	console.log("New game started. It is player 0's turn. SocketID: " + game.players[0]);
+}
+
 
 app.use(express.static(__dirname + '/assets'));
 
@@ -22,13 +32,39 @@ play.on('connection', function(socket){
   //keep track of how many users are connected
   usercount++;
   
+  //if there's less than 4 players, they're added to the player room
+  //otherwise they're added to the non player room
+  if(game.players.length < 4){
+  	game.players.push(socket.id);
+  	socket.join('players');
+  	if(game.players.length == 4){
+  	  startGame();
+  	} else {
+  	  play.to('players').emit('player update', 4 - usercount);
+  	}
+  } else {
+  	socket.join('nonplayers');
+  	play.to('nonplayers').emit('nonplayer update', usercount - 4);
+  }
+  
   //update the page to show how many users are connected
   play.emit('update', usercount);
   
   socket.on('disconnect', function(){
-  	//on disconnect, decrement the user count and update
+  	//on disconnect, decrement the user count, remove them from rooms and update
   	usercount--;
+  	var index = game.players.indexOf(socket.id);
+    if (index > -1) {
+      game.players.splice(index, 1);
+    }
     play.emit('update', usercount);
+  });
+  
+  socket.on('end turn', function(){
+  	console.log("End turn request coming from socketID: " + socket.id);
+    game.currentPlayer = (game.currentPlayer + 1) % 4;
+    play.to(game.players[game.currentPlayer]).emit('your turn', game.currentPlayer);
+    console.log("It is now player " + game.currentPlayer + "'s turn. socketID: " + game.players[game.currentPlayer]);
   });
 });
 
