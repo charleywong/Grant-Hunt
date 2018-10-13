5a55c1ddb25e411b32424204d7722a9434bb9a52
Charley Wong <charleywong@users.noreply.github.com>
2018-10-13 11:54:22 +1100

    fixed some post-merge issues

--- app.js
+++ app.js
@@ -183,7 +183,7 @@ function shuffle(deck){
 // Returns a shuffled Grant Hunt Deck
 function newDeck(){
   //preset deck for testing
-  return [8, 7, 6, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 1, 1, 1];
+  return [8, 7, 6, 5, 5, 4, 4, 2, 2, 1,3,3, 1, 1, 1, 1];
   //return shuffle([1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8]);
 }
 
