daa5110b15a8bb74ca4c3c26ea208606a3658f17
Yaminn <yaminn.aung@hotmail.com>
2018-10-16 22:42:39 +1100

    remove console log, revert deck

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ function shuffle(deck){
 // Returns a shuffled Grant Hunt Deck
 function newDeck(){
   //preset deck for testing
-  return [1, 1, 1, 1, 1, 1, 1, 1, 7, 6, 5, 5, 4, 4, 2, 2, 1, 3, 3, 1, 1, 1, 1, 8];
+  return [7, 6, 5, 5, 4, 4, 2, 2, 1, 3, 3, 1, 1, 1, 1, 8];
   //return shuffle([1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8]);
 }
 
